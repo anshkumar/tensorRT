ac895ea94d5071482c3928c77944a4790aad7821
ansh <anshgravity@gmail.com>
2020-04-08 17:54:37 +0530

    feature added

--- tfrecord_l2_self_balancing.py
+++ tfrecord_l2_self_balancing.py
@@ -4,42 +4,55 @@
 # Will only make tfrecord of jpeg images.
 ##################################
 
-import cv2
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import cv2
 import json
 import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
 from IPython import embed
 import tensorflow as tf
 from object_detection.utils import dataset_util
 import random
 
-json_path = "/Users/vedanshu/tfrecord/train_tomato.json"
-out_path = "/Users/vedanshu/tfrecord/out"
-writer = tf.python_io.TFRecordWriter("train.record")
+BASE_PATH = "/home/deploy/ved/"
+json_path = BASE_PATH+"potato_l1_l2_old_18_19_20_gid.json"
+out_path = BASE_PATH+"out"
+writer = tf.python_io.TFRecordWriter(BASE_PATH+"potato_l1_l2_old_18_19_20_gid.record")
 debug_output_img = True
 target_size=150
 ncols=5
-label_map = "/Users/vedanshu/tfrecord/label_map.pbtxt"
-write_label_map = False
+label_map = BASE_PATH+"label_map.pbtxt"
+write_label_map = True
+get_ar_as = True
+no_ar_as = 10
+only_2_cat = False # only "defective" and "normal"
+l1_mask = False # L1 in grid will be cropped masked images
 
 # category_l1 = ['tomato', 'onion', 'potato',]
 # category_l2 = [ ['spots holes', 'tip', 'cuts cracks', 'shrivelled', 'glare', 'back tip', 'stalk', 'green area', 'non black spots', "rotten", "water layer", "water mark", "deformed double", "unripe", "normal" ], 
 #      ['smut effected', 'without skin', 'sprouting', 'rotten', 'half cut', 'tip', 'neck', 'sun burn', "glare", "double onion", "open neck", "blurred" ], 
 #      ['big crack and cuts', 'decayed', 'sprouted', 'major hole', 'shriveled', 'greening', 'small crack and cuts', "badly deformed", "eyes", "confusing", "dried sprout"] ]
 
-category_l1 = ['tomato']
-category_l2 = ['tomato', 'spots_holes', 'tip', 'cuts_cracks', 'shrivelled', 'glare', 'back_tip', 'stalk', 'green_area']
+category_l1 = ['potato']
+category_l2 = ['big crack and cuts', 'decayed', 'sprouted', 'major hole', 'shriveled', 'greening', 'small crack and cuts', "badly deformed", "eyes", "confusing", "dried sprout", "small_crack_and_cuts", "badly_deformed", "big_crack_and_cuts", "major_hole", "peels", "dried_sprout"]
 
 category_index = {k:v for k,v in zip(range(1,len(category_l2)+1), category_l2)}
-defective_index = [2, 4, 5, 9]
+defective_index = [2, 4, 5]
 
 with open(json_path, 'r') as f:
     data = json.load(f)
 
 if write_label_map:
     with open(label_map, "w") as f_pbtxt:
-        for k,v in category_index.items():
-            f_pbtxt.write("item{{\n\tid: {}\n\tname: '{}'\n}}\n".format(k,v))
+        if only_2_cat:
+            f_pbtxt.write("item{\n\tid: 1\n\tname: 'defective'\n}\n")
+            f_pbtxt.write("item{\n\tid: 2\n\tname: 'normal'\n}\n")
+        else:
+            for k,v in category_index.items():
+                f_pbtxt.write("item{{\n\tid: {}\n\tname: '{}'\n}}\n".format(k,v))
 
 def checkJPG(fn):
     with tf.Graph().as_default():
@@ -86,13 +99,20 @@ def imgCrop(img, pts):
     x,y,w,h = rect
     croped = img[y:y+h, x:x+w].copy()
 
-    pts = pts - pts.min(axis=0)
+    if l1_mask:
+        pts = pts - pts.min(axis=0)
 
-    mask = np.zeros(croped.shape[:2], np.uint8)
+        mask = np.zeros(croped.shape[:2], np.uint8)
+        cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
+
+        dst = cv2.bitwise_and(croped, croped, mask=mask)
+        return dst, x, y, w, h
+    return croped, x, y, w, h
+
+def createMask(height, width, pts):
+    mask = np.zeros((height, width), np.uint8)
     cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    return dst, x, y, w, h
+    return mask
 
 class Node: 
     def __init__(self):
@@ -112,12 +132,13 @@ class Node:
         self.ymax = []
         self.classes_text = []
         self.classes = []
+        self.mask = []
 
 hierarchy_dict = {}
 ignore_filename = []
 for key in data:
-    img = cv2.imread(os.path.join("/Users/vedanshu/tfrecord/dataset", data[key]["filename"]))
-    if checkJPG(os.path.join("/Users/vedanshu/tfrecord/dataset", data[key]["filename"])):
+    img = cv2.imread(os.path.join(BASE_PATH+"dataset", data[key]["filename"]))
+    if checkJPG(os.path.join(BASE_PATH+"dataset", data[key]["filename"])):
         for j in range(len(data[key]["regions"])):
             if "parent_id" not in data[key]["regions"][j]["region_attributes"]:
                 x_lst = np.asarray(data[key]["regions"][j]["shape_attributes"]["all_points_x"])
@@ -175,11 +196,19 @@ for key in data:
                 ymin = ymin*node.scale2 + node.y2
                 ymax = ymax*node.scale2 + node.y2
 
+                pts = np.asarray([[int((x - node.x1)*node.scale2 + node.x2), int((y - node.y1)*node.scale2 + node.y2)] for x,y in zip(x_lst, y_lst)])
+
                 if xmin / node.width < 0 or xmax / node.width < 0 or ymin / node.height < 0 or ymax / node.height < 0:
                     print("...ONE CONTOUR SKIPPED...")
                     continue
                 if xmin / node.width > 1 or xmax / node.width > 1 or ymin / node.height > 1 or ymax / node.height > 1:
                     print("...ONE CONTOUR SKIPPED...")
+                    continue
+
+                try:
+                    mask = createMask(node.height, node.width, pts)
+                except:
+                    print("Invalid mask...")
                     continue
                 
                 txt = data[key]["regions"][j]["region_attributes"][category_l1[0]]
@@ -194,13 +223,19 @@ for key in data:
                 node.xmax.append(xmax / node.width)
                 node.ymin.append(ymin / node.height)
                 node.ymax.append(ymax / node.height) 
+                node.mask.append(mask)
 
-                if index in defective_index:
-                    node.classes_text.append(tf.compat.as_bytes("defective"))
-                    node.classes.append(1)
+                if only_2_cat:
+                    if index in defective_index:
+                        node.classes_text.append(tf.compat.as_bytes("defective"))
+                        node.classes.append(1)
+                    else:
+                        node.classes_text.append(tf.compat.as_bytes("normal"))
+                        node.classes.append(2)
                 else:
-                    node.classes_text.append(tf.compat.as_bytes("normal"))
-                    node.classes.append(2)
+                    node.classes_text.append(tf.compat.as_bytes(txt))
+                    node.classes.append(index)
+
 
 def gallery(array, ncols=5):
     # https://stackoverflow.com/a/42041135/4582711
@@ -222,16 +257,25 @@ lst_id = [_id for _id, node in hierarchy_dict.items() if len(node.xmin) > 0]
 not_ended = True
 start_index = 0
 
+lst_xmins = []
+lst_xmaxs = []
+lst_ymins = []
+lst_ymaxs = []
+lst_width = []
+lst_height = []
+lst_classes = []
+
 while not_ended:
-    grid_img = np.zeros((25,150,150,3))
+    grid_img = np.zeros((ncols*ncols,target_size,target_size,3))
     xmins = []
     ymins = []
     xmaxs = []
     ymaxs = []
     classes_text = []
     classes = []
+    encoded_jpg_mask_lst = []
 
-    rand_size = random.randint(1,25)
+    rand_size = random.randint(1,ncols*ncols)
     normalizer = ncols*target_size
     
     for i in range(rand_size):
@@ -246,8 +290,14 @@ while not_ended:
         img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         grid_img[i] = img_np
 
-        for xmin, ymin, xmax, ymax, txt, _index in zip(node.xmin, node.ymin, node.xmax, node.ymax, node.classes_text, node.classes):
+        for xmin, ymin, xmax, ymax, txt, _index, _mask in zip(node.xmin, node.ymin, node.xmax, node.ymax, node.classes_text, node.classes, node.mask):
             _x, _y = getShiftingCord(i, ncols)
+
+            grid_img_mask = np.zeros((normalizer,normalizer))
+            grid_img_mask[_y:(_y+_mask.shape[0]), _x:(_x+_mask.shape[1])] = _mask
+            grid_img_mask = cv2.imencode('.png', grid_img_mask)[1].tostring()
+            encoded_jpg_mask_lst.append(grid_img_mask)
+            
             xmins.append( (xmin*node.width + _x)/normalizer)
             ymins.append( (ymin*node.height + _y)/normalizer)
             xmaxs.append( (xmax*node.width + _x)/normalizer)
@@ -255,32 +305,67 @@ while not_ended:
             classes_text.append(txt)
             classes.append(_index)
 
+            lst_xmins.append((xmin*node.width + _x))
+            lst_xmaxs.append((xmax*node.height + _x))
+            lst_ymins.append((ymin*node.width + _y))
+            lst_ymaxs.append((ymax*node.height + _y))
+            lst_width.append(normalizer)
+            lst_height.append(normalizer)
+            lst_classes.append(_index)
+
     grid_img = gallery(grid_img, ncols)
-    encoded_jpg = cv2.imencode('.jpg', _img)[1].tostring()
+    encoded_jpg = cv2.imencode('.jpg', grid_img)[1].tostring()
 
     if debug_output_img:
-        for xmin, ymin, xmax, ymax, txt in zip(xmins, ymins, xmaxs, ymaxs, classes_text):
+        for xmin, ymin, xmax, ymax, txt, _mask in zip(xmins, ymins, xmaxs, ymaxs, classes_text, encoded_jpg_mask_lst):
             cv2.rectangle(grid_img, (int(xmin*normalizer), int(ymin*normalizer)), (int(xmax*normalizer), int(ymax*normalizer)), (255,0,0), 2)
             cv2.putText(grid_img, str(txt), (int(xmin*normalizer), int(ymin*normalizer)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
-
+            nparr = np.fromstring(_mask, np.uint8) 
+            mask_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+            _bgd_mask = cv2.bitwise_not(mask_np)
+            _bgd = cv2.bitwise_and(grid_img,grid_img,mask = _bgd_mask)
+            roi = cv2.bitwise_and(grid_img,grid_img,mask = mask_np)
+            _roi = roi*[0,0,0.6]
+            grid_img = _bgd + _roi
         if not_ended:
             _path = os.path.join(out_path, str(lst_id[start_index+i]) +"_ann.jpg")
             cv2.imwrite(_path, grid_img)
-    else:    
-        tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(normalizer),
-        'image/width': dataset_util.int64_feature(normalizer),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(b'jpg'),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-        }))
-        writer.write(tf_example.SerializeToString())
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+    'image/height': dataset_util.int64_feature(normalizer),
+    'image/width': dataset_util.int64_feature(normalizer),
+    'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+    'image/format': dataset_util.bytes_feature(b'jpg'),
+    'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+    'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+    'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+    'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+    'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+    'image/object/class/label': dataset_util.int64_list_feature(classes),
+    'image/object/mask': dataset_util.bytes_list_feature(encoded_jpg_mask_lst)
+    }))
+    writer.write(tf_example.SerializeToString())
     start_index = start_index + i + 1
+
+if get_ar_as:
+    data = pd.DataFrame({'xmins': lst_xmins, 'ymins': lst_ymins, 'xmaxs': lst_xmaxs, 'ymaxs': lst_ymaxs, 'width': lst_width, 'height': lst_height})
+    data['w'] = data['xmaxs'] - data['xmins']
+    data['h'] = data['ymaxs'] - data['ymins']
+
+    data['b_w'] = target_size*data['w']/data['width']
+    data['b_h'] = target_size*data['h']/data['height']
+
+    X = data.as_matrix(columns=['b_w', 'b_h'])
+    K = KMeans(no_ar_as, random_state=0)
+    labels = K.fit(X)
+
+    out = labels.cluster_centers_
+
+    ar = out[:,0]/out[:,1]
+    scale = out[:,1]*np.sqrt(ar)/256
+
+    print("Aspect Ratios: ",ar)
+
+    print("Scales: ", scale)
 
 writer.close()
 
